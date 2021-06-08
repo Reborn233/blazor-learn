55ed3e25680366f59e698629fcb60c2bf300b087
Reborn233 <499289113@qq.com>
2021-06-08 11:31:55 +0800

    create blazor app

---
+++ Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿public class TodoItem
+{
+  public TodoItem() { }
+  public TodoItem(int id, string title, bool isDone)
+  {
+    Id = id;
+    Title = title;
+    IsDone = isDone;
+  }
+  public int Id { get; set; }
+  public string Title { get; set; }
+  public bool IsDone { get; set; }
+}
